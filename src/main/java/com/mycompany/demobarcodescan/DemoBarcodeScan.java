2d7ee7f0ca1e85326e290b53be1f580f7d0ab368
PhamVanTea <23633471.tra@student.iuh.edu.vn>
2025-08-21 12:21:09 +0700

    initial commit

---
+++ src/main/java/com/mycompany/demobarcodescan/DemoBarcodeScan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.demobarcodescan;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class DemoBarcodeScan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
